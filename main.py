6d85ba11a917c2d7ae2e9d2ebd638fdba6777784
gefrefifth 22 <gefrefifth22@gmail.com>
2020-10-29 19:43:36 +0000

    code

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ while whiler > 0 :
   W = int(input("Enter Width =>"))
   X = int(input("Enter X =>"))
   Y = int(input("Enter Y =>"))
-  Clean = int(input("Clean or not? (0 or 1)"))
+  Clean = int(input("Clean or not? (0 or 1) =>"))
   if Clean == 1 :
     a.reset()
   a.width(W)
