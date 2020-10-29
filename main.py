1f3fbbe56bb9d6d07e5497936caac3b8a1978356
gefrefifth 22 <gefrefifth22@gmail.com>
2020-10-29 19:36:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import turtle
+whiler = 1
+a = turtle.Turtle()
+a.width(1)
+while whiler > 0 :
+  W = int(input("Enter Width =>"))
+  X = int(input("Enter X =>"))
+  Y = int(input("Enter Y =>"))
+  Clean = int(input("Clean or not? (0 or 1)"))
+  if Clean == 1 :
+    a.reset()
+  a.width(W)
+  a.goto(X, Y)
+  print("done!")
